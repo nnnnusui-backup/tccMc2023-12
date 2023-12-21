be05c6437487f8f04e2a329df7fbc740eda23be8
nnnnusui-backup <nnnnusui+backup@gmail.com>
2023-12-22 08:23:19 +0900

    client mods list

---
+++ mods-client.sh
@@ -0,0 +1,18 @@
+out_path="mods-client.txt"
+src_dir="/mnt/c/_/game/minecraft/modrinth/profiles/1.20.1 TCC v1.0.1/mods"
+tmp_dir="client-mods-tmp"
+
+echo "rm tmp[$tmp_dir] ..."
+rm -rf "$tmp_dir"
+
+echo "cp from[$src_dir] ..."
+cp -r "$src_dir" "$tmp_dir"
+
+echo "gen to[$out_path] ..."
+read_dir="$tmp_dir"
+ls -1 "$read_dir" > "$out_path"
+
+rm -rf "$tmp_dir"
+
+echo "done."
+
